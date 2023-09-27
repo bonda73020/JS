74f59863840eacc20225ee17c0109eb3e809b8c3
bonda73020 <myronetsb@gmail.com>
2023-09-27 09:58:08 +0300

    Created index.html and main.js + added variables and output

---
+++ Lesson1/main.js
@@ -0,0 +1,27 @@
+let s1 = "hello"
+let s2 = "owu"
+let s3 = "com"
+let s4 = "ua"
+let n1 = 1
+let n2 = 10
+let n3 = -999
+let n4 = 123
+let f1 = 3.14
+let f2 = 2.7
+let n5 = 16
+let b1 = true
+let b2 = false
+
+console.log(s1)
+console.log(s2)
+console.log(s3)
+console.log(s4)
+console.log(n1)
+console.log(n2)
+console.log(n3)
+console.log(n4)
+console.log(f1)
+console.log(f2)
+console.log(n5)
+console.log(b1)
+console.log(b2)
