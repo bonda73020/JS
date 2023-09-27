b7ce27feb1e05427464b0c112d2b439d0f042256
bonda73020 <myronetsb@gmail.com>
2023-09-27 10:26:46 +0300

    added typeof for a,b,c variables

--- Lesson1/main.js
+++ Lesson1/main.js
@@ -32,3 +32,12 @@ let middleName = "Миронець"
 let lastName = "Вікторович"
 
 console.log(middleName+" "+firstNam+ " " + lastName)
+
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log("type of a: "+ typeof a)
+console.log("type of b: "+ typeof b)
+console.log("type of c: "+ typeof c)
