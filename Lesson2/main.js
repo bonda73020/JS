308cac8a0d160e7e8ccda9d032b9cf2c4e1c25f3
bonda73020 <myronetsb@gmail.com>
2023-09-27 10:35:22 +0300

    added 3 book objects (title ,pageCount, genre)

--- Lesson2/main.js
+++ Lesson2/main.js
@@ -1,3 +1,23 @@
 let a1 = [1,2,3,"4","5","6","7","8",true,false]
 
 console.log(a1)
+
+
+let book1 = {
+    title: "Fifty shades of grey",
+    pageCount: 514,
+    genre: "Erotic"
+};
+
+
+let book2 = {
+    title: "Looking for alaska",
+    pageCount: 272,
+    genre: "young adult novel"
+};
+
+let book3 = {
+    title: "The Hound of the Baskervilles",
+    pageCount: 200,
+    genre: "Detective novel"
+};
